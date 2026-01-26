5a4194ca9f0547d8cf66d6c5812d55ab2413ce62
Alfred Bartholomew Sunday <alfred.bartholomewsunday@gmail.com>
2026-01-26 16:50:52 +0000

    Add valid parentheses solution

---
+++ valid_bracs.py
@@ -0,0 +1,79 @@
+'''
+20. Valid Parentheses
+Easy
+Topics
+premium lock icon
+Companies
+Hint
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+ 
+
+Example 1:
+
+Input: s = "()"
+
+Output: true
+
+Example 2:
+
+Input: s = "()[]{}"
+
+Output: true
+
+Example 3:
+
+Input: s = "(]"
+
+Output: false
+
+Example 4:
+
+Input: s = "([])"
+
+Output: true
+
+Example 5:
+
+Input: s = "([)]"
+
+Output: false
+
+ 
+
+Constraints:
+
+1 <= s.length <= 104
+s consists of parentheses only '()[]{}'.
+
+'''
+s = "()[]{}"
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack = []
+        for c in s:
+            if c == '(' or c == '{' or c == '[':
+                stack.append(c)
+            else:
+                if not stack:
+                    return False
+                last = stack.pop()
+                if c == ')' and last != '(':
+                    return False
+                if c == '}' and last != '{':
+                    return False
+                if c == ']' and last != '[':
+                    return False
+        return not stack
+    
+sol = Solution()
+print(sol.isValid(s))
